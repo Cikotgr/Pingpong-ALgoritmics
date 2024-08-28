5387ace643b1f1f29a2c1c8ff6ad572b508e0dfe
Cikotgr <cikotegar.s@gmail.com>
2024-08-28 22:01:32 +0700

    racket2 update

--- ping_pong.py
+++ ping_pong.py
@@ -48,7 +48,7 @@ FPS = 60
 #create ball and  racket
 
 racket1 = Player('racket.png',30,200,4,50,150)
-racket2 = Player('racket.png',520,200,4,50,150)
+racket2 = Player('racket.png',520,200,4,50,100)
 ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
 
 font.init()
